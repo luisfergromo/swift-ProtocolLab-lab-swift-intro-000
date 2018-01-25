c4896b1e389e961cb12a09dd00917ccedeb35425
Luis Fernando Gutiérrez Romo <changoluis@hotmail.com>
2018-01-25 11:55:55 -0600

    Compilation error

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -5,20 +5,42 @@
 //  Created by Papa Roach on 8/8/16.
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
+class BankVault{
 
-
-class BankVault {
-    
     let name: String
     let address: String
     var amount: Double = 0.0
-    
+
     init(name: String, address: String) {
         self.name = name
         self.address = address
     }
-    
+
 }
+protocol ProvideAccess {
+    func allowEntryWithPassword(password:[Int]) -> Bool
+}
+extension BankVault:ProvideAccess{
+    func allowEntryWithPassword(password:[Int]) -> Bool{
+        var allowEntry = true
+        if password.isEmpty{
+            allowEntry = false
+        }
+        if password.count > 10{
+            allowEntry = false
+        }
+        for (index,digit) in password.enumerated(){
+            if index % 2 == 0{
+                if digit % 2 != 0{
+                    allowEntry = false
+                }
+            }
+        }
+        return allowEntry
+    }
+}
+
+
 
 
 
